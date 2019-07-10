109706f60ae20a5e300663412ad9881c38a813d8
Adriell <acdeguzman@codedisruptors.com>
2019-07-11 01:00:20 +0800

    Prompt resource not found for invalid routes

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(body_parser.urlencoded({extended: false}));
 /* Connect router */
 app.use('/api/v1', require('./config/router')(express.Router()));
 
-app.get('/api/v1', (req, res) => {
-
-    return res.send('Welcome to Sample CRUD API');
-})
-
-app.use('/', (req, res) => {
+app.use('(/)', (req, res) => {
 
     return res.redirect('/api/v1');
+});
+
+app.use((req, res, next) => {
+
+    return res.send('Resource not found');
 });
 
 
