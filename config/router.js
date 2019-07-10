4d32eca76cca7ffb9b7d199e82794ba9577f828b
Adriell <acdeguzman@codedisruptors.com>
2019-07-11 00:34:10 +0800

    Add route and controller for POST items

--- config/router.js
+++ config/router.js
@@ -5,6 +5,7 @@ const items_controller = require(__dirname + '/../controller/items');
 module.exports = router => {
 
     router.get('/items',    items_controller.get_items);
+    router.post('/items',   items_controller.add_item);
 
     return router;
 }
--- controller/items.js
+++ controller/items.js
@@ -10,6 +10,24 @@ exports.get_items = (req, res) => {
 
         if(err) return err;
 
-        return res.send(result);
+        return res.send({data: result});
+    });
+}
+
+exports.add_item = (req, res) => {
+
+    const {
+        name,
+        qty,
+        amount
+    } = req.body;
+
+    const query = `INSERT INTO items (name, qty, amount) values ('${name}', ${qty}, ${amount})`;
+
+    db.query(query, (err, result, fields) => {
+
+        if(err) return res.send(err);
+
+        return res.send({data: {message: "Item added!"}});
     });
 }
