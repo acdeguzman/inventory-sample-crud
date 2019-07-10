dc512783321808aa2b248fb846412dc9aad90b98
Adriell <acdeguzman@codedisruptors.com>
2019-07-11 00:22:35 +0800

    Add body-parser

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const body_parser = require('body-parser');
+
+app.use(body_parser.urlencoded({extended: false}));
 
 /* Connect router */
 app.use('/api/v1', require('./config/router')(express.Router()));
