a1c1ce3040f3509ddf82631d0948282fa62371c2
Adriell <acdeguzman@codedisruptors.com>
2019-07-11 00:13:09 +0800

    Connect router to index

--- index.js
+++ index.js
@@ -3,14 +3,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const mysql = require('mysql');
+
+/* Connect router */
+app.use('/api/v1', require('./config/router')(express.Router()));
+
+app.get('/api/v1', (req, res) => {
+
+    return res.send('Welcome to Sample CRUD API');
+})
+
+app.use('/', (req, res) => {
+
+    return res.redirect('/api/v1');
+});
+
 
 app.listen(port, () => {
 
     console.log(`Listening at port ${port}`);
-});
-
-app.get('/', (req, res) => {
-
-    res.send('Sample Inventory CRUD landing page');
 });
