e235cfe8f98be9d9f8c7e9cc8c441836ab9a466c
Adriell <acdeguzman@codedisruptors.com>
2019-07-10 23:12:04 +0800

    Finished setup of package.json

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql');
+
+app.listen(port, () => {
+
+    console.log(`Listening at port ${port}`);
+});
