f7fc619875ea6a58e22072b8ae3211a5152eeb7e
Adriell <acdeguzman@codedisruptors.com>
2019-07-12 01:54:45 +0800

    Change PUT request of update item to POST for frontend integration

--- config/router.js
+++ config/router.js
@@ -4,12 +4,11 @@ const items_controller = require(__dirname + '/../controller/items');
 
 module.exports = router => {
 
-    router.get('/items',        items_controller.get_items);
-    router.get('/items/:id',    items_controller.get_item);
-    router.post('/items',       items_controller.add_item);
-    router.put('/items/:id',    items_controller.update_item);
-    router.delete('/items/:id', items_controller.delete_item);
-    router.get('/items_add',    items_controller.add_item_ui);
-
+    router.get('/items',            items_controller.get_items);
+    router.get('/items/:id',        items_controller.get_item);
+    router.post('/items',           items_controller.add_item);
+    router.post('/items/:id', items_controller.update_item);
+    router.delete('/items/:id',     items_controller.delete_item);
+    router.get('/items_add',        items_controller.add_item_ui);
     return router;
 }
