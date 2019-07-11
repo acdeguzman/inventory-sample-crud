97155848a7df9ae0035e93bffa03763b696d3c4b
Adriell <acdeguzman@codedisruptors.com>
2019-07-12 02:10:40 +0800

    Change route of DELETE item

--- config/router.js
+++ config/router.js
@@ -7,8 +7,8 @@ module.exports = router => {
     router.get('/items',            items_controller.get_items);
     router.get('/items/:id',        items_controller.get_item);
     router.post('/items',           items_controller.add_item);
-    router.post('/items/:id', items_controller.update_item);
-    router.delete('/items/:id',     items_controller.delete_item);
+    router.post('/items/:id',       items_controller.update_item);
+    router.get('/delete_item/:id',  items_controller.delete_item);
     router.get('/items_add',        items_controller.add_item_ui);
     return router;
 }
